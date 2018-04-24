ff922de8208dde3cf3f7982a9d02b492dc8944b5
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2018-04-24 22:46:13 +0200

    ~ reficio plugin

--- .travis/artifact-upload.sh
+++ .travis/artifact-upload.sh
@@ -5,7 +5,7 @@ target_dir=slizaa-thirdparty-dependencies
 
 echo TRAVIS_BUILD_DIR: $TRAVIS_BUILD_DIR
 
-PRODUCTS_DIR=$TRAVIS_BUILD_DIR/org.slizaa.thirdparty.p2/target/repository
+PRODUCTS_DIR=$TRAVIS_BUILD_DIR/target/repository
 echo PRODUCTS_DIR: $PRODUCTS_DIR
 
 filesToUpload=$(find $PRODUCTS_DIR -name "*.jar" -printf '%P\n')
